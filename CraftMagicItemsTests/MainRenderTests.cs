e25f8115b386527a2f1747ec0988fc21ece690ea
Erik Frantz <bardez@bardez.net>
2020-06-26 11:46:58 -0500

    Adding in an empty unit test class

---
+++ CraftMagicItemsTests/MainRenderTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CraftMagicItems;
+
+namespace CraftMagicItemsTests
+{
+    [TestClass]
+    public class MainRenderTests
+    {
+        [TestMethod]
+        public void RenderMultiLayerSkills_Works()
+        {
+        }
+    }
+}
