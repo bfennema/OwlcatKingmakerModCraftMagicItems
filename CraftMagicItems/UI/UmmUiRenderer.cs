aae548ce73f5cd7b30f95b235d2739cb574d35a5
Erik Frantz <bardez@bardez.net>
2020-06-26 11:55:08 -0500

    Flipping ref to a return value

--- CraftMagicItems/UI/UmmUiRenderer.cs
+++ CraftMagicItems/UI/UmmUiRenderer.cs
@@ -13,7 +13,7 @@ namespace CraftMagicItems.UI
         /// <summary>Renders a checkbox in Unity Mod Manager</summary>
         /// <param name="value">Currently selected value</param>
         /// <param name="label">Label for the checkbox</param>
-        public static void RenderCheckbox(ref bool value, string label)
+        public static bool RenderCheckbox(bool value, string label)
         {
             GUILayout.BeginHorizontal();
             if (GUILayout.Button($"{(value ? "<color=green><b>✔</b></color>" : "<color=red><b>✖</b></color>")} {label}", GUILayout.ExpandWidth(false)))
@@ -22,6 +22,8 @@ namespace CraftMagicItems.UI
             }
 
             GUILayout.EndHorizontal();
+
+            return value;
         }
 
         /// <summary>Renders a text box on screen in Unity Mod Manager for the item's customized name</summary>
