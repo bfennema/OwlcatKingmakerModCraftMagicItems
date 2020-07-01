c8a0ec1aa259902b6657044059a89d0f0b15c32d
Erik Frantz <bardez@bardez.net>
2020-07-01 15:31:30 -0500

    Adding in unit tests over CheatSectionRendererFactory

---
+++ CraftMagicItemsTests/UI/Sections/CheatSectionFactoryTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using CraftMagicItems.UI.Sections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace CraftMagicItemsTests.UI.Sections
+{
+    /// <summary>Test class for <see cref="CheatSectionRendererFactory" /></summary>
+    [TestClass]
+    public class CheatSectionRendererFactoryTests
+    {
+        [TestMethod]
+        public void CheatSectionRendererFactory_Defaults_CheatSectionRenderer()
+        {
+            //control
+            CheatSectionRendererFactory.Reset();
+
+            //invocation
+            var instance = CheatSectionRendererFactory.GetCheatSectionRenderer();
+
+            //validation
+            Assert.AreEqual(typeof(CheatSectionRenderer), instance.GetType(), $"Expected an instance of {nameof(CheatSectionRenderer)} to be returned.");
+        }
+
+        [TestMethod]
+        public void SetConstructor_Works()
+        {
+            //control
+            Func<ICheatSectionRenderer> mockConstructor = () => { return new Mock<ICheatSectionRenderer>().Object; };
+            CheatSectionRendererFactory.SetConstructor(mockConstructor);
+
+            //invocation
+            var instance = CheatSectionRendererFactory.GetCheatSectionRenderer();
+
+            //validation
+            Assert.AreNotEqual(typeof(CheatSectionRenderer), instance.GetType(), $"Expected an instance of {nameof(CheatSectionRenderer)} to be returned.");
+        }
+
+        [TestMethod]
+        public void GetCheatSectionRenderer_Works()
+        {
+            //invocation
+            var instance = CheatSectionRendererFactory.GetCheatSectionRenderer();
+
+            //validation
+            Assert.IsNotNull(instance, $"Expected an instance of {nameof(ICheatSectionRenderer)} to be returned.");
+        }
+
+        [TestMethod]
+        public void Reset_Works()
+        {
+            //control
+            Func<ICheatSectionRenderer> mockConstructor = () => { return new Mock<ICheatSectionRenderer>().Object; };
+            CheatSectionRendererFactory.SetConstructor(mockConstructor);
+            CheatSectionRendererFactory.Reset();
+
+            //invocation
+            var instance = CheatSectionRendererFactory.GetCheatSectionRenderer();
+
+            //validation
+            Assert.AreEqual(typeof(CheatSectionRenderer), instance.GetType(), $"Expected an instance of {nameof(CheatSectionRenderer)} to be returned.");
+        }
+    }
+}
