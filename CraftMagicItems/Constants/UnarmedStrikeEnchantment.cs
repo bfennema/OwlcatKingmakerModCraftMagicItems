0563c571889bba7fc59a66033e605bf7bcc1a5da
Erik Frantz <bardez@bardez.net>
2020-07-02 14:32:03 -0500

    Adding in a struct to replace/convert the `EnchantmentBlueprints.ItemEnchantmentGuids` string array

---
+++ CraftMagicItems/Constants/UnarmedStrikeEnchantment.cs
@@ -0,0 +1,15 @@
+﻿namespace CraftMagicItems.Constants
+{
+    /// <summary>Structure defining enchantments that exist for both melee and unarmed strikes</summary>
+    public struct UnarmedStrikeEnchantment
+    {
+        /// <summary>Used for descriptive purposes rather than a comment in code that someone might delete</summary>
+        public string Description;
+
+        /// <summary>The unique identifier of the weapon enchantment blueprint to copy data from</summary>
+        public string WeaponEnchantmentGuid;
+
+        /// <summary>The unique identifier of the unarmed strike blueprint to copy data into</summary>
+        public string UnarmedEnchantmentGuid;
+    }
+}
