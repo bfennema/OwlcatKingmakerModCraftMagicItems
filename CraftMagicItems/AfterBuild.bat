e463359516c094917800290ac44f1b5edc610446
Erik Frantz <bardez@bardez.net>
2020-07-16 13:29:59 -0500

    Moving the data into the empty Data dir already in the solution

--- CraftMagicItems/AfterBuild.bat
+++ CraftMagicItems/AfterBuild.bat
@@ -6,10 +6,10 @@ xcopy CraftMagicItems.dll CraftMagicItems || goto :error
 xcopy ..\..\..\Info.json CraftMagicItems || goto :error
 xcopy /E /I ..\..\..\L10n CraftMagicItems\L10n || goto :error
 xcopy /E /I ..\..\..\Icons CraftMagicItems\Icons || goto :error
-xcopy /E /I ..\..\..\Data CraftMagicItems\Data || goto :error
+xcopy /E /I Data CraftMagicItems\Data || goto :error
 "C:\Program Files\7-Zip\7z.exe" a CraftMagicItems.zip CraftMagicItems || goto :error
 
-"C:\Program Files\7-Zip\7z.exe" a CraftMagicItems-Source.zip ..\..\*.cs ..\..\..\L10n ..\..\..\Icons ..\..\..\Data || goto :error
+"C:\Program Files\7-Zip\7z.exe" a CraftMagicItems-Source.zip ..\..\*.cs ..\..\..\L10n ..\..\..\Icons Data || goto :error
 
 goto :EOF
 
