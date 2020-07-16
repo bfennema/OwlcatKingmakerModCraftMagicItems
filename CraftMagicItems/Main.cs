17a1d372ea53f464a1c797276694db73e051f76a
Erik Frantz <bardez@bardez.net>
2020-07-16 15:35:41 -0500

    Moving IkPatches (?) into their own constants, namespace, and file

--- CraftMagicItems/Main.cs
+++ CraftMagicItems/Main.cs
@@ -7,6 +7,7 @@ using System.Reflection.Emit;
 using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using CraftMagicItems.Constants;
+using CraftMagicItems.Patches;
 using CraftMagicItems.UI;
 using CraftMagicItems.UI.Sections;
 using CraftMagicItems.UI.UnityModManager;
@@ -110,15 +111,6 @@ namespace CraftMagicItems {
             Stash
         }
 
-        struct IkPatch {
-            public IkPatch(string uuid, float x, float y, float z) {
-                m_uuid = uuid;
-                m_x = x; m_y = y; m_z = z;
-            }
-            public string m_uuid;
-            public float m_x, m_y, m_z;
-        }
-
         struct MethodPatch {
             public MethodPatch(MethodBase original, HarmonyLib.HarmonyMethod prefix = null, HarmonyLib.HarmonyMethod postfix = null) {
                 m_original = original;
@@ -156,18 +148,6 @@ namespace CraftMagicItems {
             new MethodPatch(
                 typeof(Player).GetMethod("PostLoad"),
                 postfix: new HarmonyLib.HarmonyMethod(typeof(PlayerPostLoadPatch).GetMethod("Postfix", BindingFlags.NonPublic | BindingFlags.Static))),
-        };
-
-        private static readonly IkPatch[] IkPatchList = {
-            new IkPatch("a6f7e3dc443ff114ba68b4648fd33e9f",  0.00f, -0.10f, 0.01f), // dueling sword
-            new IkPatch("13fa38737d46c9e4abc7f4d74aaa59c3",  0.00f, -0.36f, 0.00f), // tongi
-            new IkPatch("1af5621e2ae551e42bd1dd6744d98639",  0.00f, -0.07f, 0.00f), // falcata
-            new IkPatch("d516765b3c2904e4a939749526a52a9a",  0.00f, -0.15f, 0.00f), // estoc
-            new IkPatch("2ece38f30500f454b8569136221e55b0",  0.00f, -0.08f, 0.00f), // rapier
-            new IkPatch("a492410f3d65f744c892faf09daad84a",  0.00f, -0.20f, 0.00f), // heavy pick
-            new IkPatch("6ff66364e0a2c89469c2e52ebb46365e",  0.00f, -0.10f, 0.00f), // trident
-            new IkPatch("d5a167f0f0208dd439ec7481e8989e21",  0.00f, -0.08f, 0.00f), // heavy mace
-            new IkPatch("8fefb7e0da38b06408f185e29372c703", -0.14f,  0.00f, 0.00f), // heavy flail
         };
 
         public static UnityModManager.ModEntry ModEntry;
@@ -3254,10 +3234,11 @@ namespace CraftMagicItems {
             }
 
             private static void PatchIk() {
-                foreach (var patch in IkPatchList) {
+                foreach (var patch in VisualAdjustmentPatches.IkPatchList) {
                     var weapon = ResourcesLibrary.TryGetBlueprint<BlueprintWeaponType>(patch.m_uuid);
                     if (weapon != null) {
-                        var model = weapon.VisualParameters.Model; var equipmentOffsets = model.GetComponent<EquipmentOffsets>();
+                        var model = weapon.VisualParameters.Model;
+                        var equipmentOffsets = model.GetComponent<EquipmentOffsets>();
                         var locator = new GameObject();
                         locator.transform.SetParent(model.transform);
                         locator.transform.localPosition = new Vector3(patch.m_x, patch.m_y, patch.m_z);
---
+++ CraftMagicItems/Patches/IkPatch.cs
@@ -0,0 +1,23 @@
+﻿namespace CraftMagicItems.Patches
+{
+    /// <summary>Structure defining visual X/Y/Z-axis adjustments to weapons to patch the existing game</summary>
+    public struct IkPatch
+    {
+        /// <summary>Constructor</summary>
+        /// <param name="uuid">Unique ID of the blueprint to patch</param>
+        /// <param name="x">X-axis adjustment</param>
+        /// <param name="y">Y-axis adjustment</param>
+        /// <param name="z">Z-axis adjustment</param>
+        public IkPatch(string uuid, float x, float y, float z)
+        {
+            m_uuid = uuid;
+            m_x = x;
+            m_y = y;
+            m_z = z;
+        }
+
+        public string m_uuid;
+        
+        public float m_x, m_y, m_z;
+    }
+}
