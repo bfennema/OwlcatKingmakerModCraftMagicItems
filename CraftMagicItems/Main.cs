f397c6eb2abbce39267b5eec56e40391cfd90826
Erik Frantz <bardez@bardez.net>
2020-12-14 13:00:09 -0600

    Extracting the Harmony patches from Main into their own files: AttackStatReplacementOnEventAboutToTriggerPatch

--- CraftMagicItems/Main.cs
+++ CraftMagicItems/Main.cs
@@ -3333,19 +3333,6 @@ namespace CraftMagicItems {
             }
         }
 
-        [HarmonyLib.HarmonyPatch(typeof(AttackStatReplacement), "OnEventAboutToTrigger")]
-        // ReSharper disable once UnusedMember.Local
-        private static class AttackStatReplacementOnEventAboutToTriggerPatch {
-            private static bool Prefix(AttackStatReplacement __instance, RuleCalculateAttackBonusWithoutTarget evt) {
-                if (evt.Weapon != null && __instance.SubCategory == WeaponSubCategory.Finessable &&
-                    evt.Weapon.Blueprint.Type.Category.HasSubCategory(WeaponSubCategory.Finessable) && IsOversized(evt.Weapon.Blueprint)) {
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-        }
-
         [HarmonyLib.HarmonyPatch(typeof(DamageGrace), "OnEventAboutToTrigger")]
         // ReSharper disable once UnusedMember.Local
         private static class DamageGraceOnEventAboutToTriggerPatch {
---
+++ CraftMagicItems/Patches/Harmony/AttackStatReplacementOnEventAboutToTriggerPatch.cs
@@ -0,0 +1,26 @@
+﻿using Kingmaker.Enums;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.UnitLogic.FactLogic;
+
+namespace CraftMagicItems.Patches.Harmony
+{
+    [HarmonyLib.HarmonyPatch(typeof(AttackStatReplacement), "OnEventAboutToTrigger")]
+    // ReSharper disable once UnusedMember.Local
+    public static class AttackStatReplacementOnEventAboutToTriggerPatch
+    {
+        private static bool Prefix(AttackStatReplacement __instance, RuleCalculateAttackBonusWithoutTarget evt)
+        {
+            if (evt.Weapon != null
+                && __instance.SubCategory == WeaponSubCategory.Finessable
+                && evt.Weapon.Blueprint.Type.Category.HasSubCategory(WeaponSubCategory.Finessable) &&
+                Main.IsOversized(evt.Weapon.Blueprint))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
