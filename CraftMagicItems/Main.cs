dfcc393e743479e47b972218bcc90749753b645c
Erik Frantz <bardez@bardez.net>
2020-12-14 13:19:06 -0600

    Extracting the Harmony patches from Main into their own files: WeaponExtraAttackOnEventAboutToTriggerPatch

--- CraftMagicItems/Main.cs
+++ CraftMagicItems/Main.cs
@@ -3342,25 +3342,6 @@ namespace CraftMagicItems {
             }
         }
 
-        [HarmonyLib.HarmonyPatch(typeof(WeaponExtraAttack), "OnEventAboutToTrigger")]
-        // ReSharper disable once UnusedMember.Local
-        private static class WeaponExtraAttackOnEventAboutToTriggerPatch {
-            // ReSharper disable once UnusedMember.Local
-            private static bool Prefix(WeaponExtraAttack __instance, RuleCalculateAttacksCount evt) {
-                if (__instance is ItemEnchantmentLogic logic) {
-                    if (logic.Owner is ItemEntityWeapon) {
-                        evt.AddExtraAttacks(__instance.Number, __instance.Haste, __instance.Owner);
-                    } else if (evt.Initiator.GetFirstWeapon() != null &&
-                        (evt.Initiator.GetFirstWeapon().Blueprint.IsNatural || evt.Initiator.GetFirstWeapon().Blueprint.IsUnarmed)) {
-                        evt.AddExtraAttacks(__instance.Number, __instance.Haste);
-                    }
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-        }
-
         [HarmonyLib.HarmonyPatch(typeof(WeaponDamageAgainstAlignment), "OnEventAboutToTrigger")]
         // ReSharper disable once UnusedMember.Local
         private static class WeaponDamageAgainstAlignmentOnEventAboutToTriggerPatch {
