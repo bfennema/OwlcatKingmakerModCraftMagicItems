d30731a8cba7e616b7412bcd7083008f750921d3
Erik Frantz <bardez@bardez.net>
2020-12-14 13:17:49 -0600

    Extracting the Harmony patches from Main into their own files: WeaponEnergyBurstOnEventAboutToTriggerPatch

--- CraftMagicItems/Main.cs
+++ CraftMagicItems/Main.cs
@@ -3342,27 +3342,6 @@ namespace CraftMagicItems {
             }
         }
 
-        [HarmonyLib.HarmonyPatch(typeof(WeaponEnergyBurst), "OnEventAboutToTrigger")]
-        // ReSharper disable once UnusedMember.Local
-        private static class WeaponEnergyBurstOnEventAboutToTriggerPatch {
-            // ReSharper disable once UnusedMember.Local
-            private static bool Prefix(WeaponEnergyBurst __instance, RuleDealDamage evt) {
-                if (__instance is ItemEnchantmentLogic logic) {
-                    if (logic.Owner == null || evt.AttackRoll == null || !evt.AttackRoll.IsCriticalConfirmed || evt.AttackRoll.FortificationNegatesCriticalHit || evt.DamageBundle.WeaponDamage == null) {
-                        return false;
-                    }
-                    if (EquipmentEnchantmentValid(evt.DamageBundle.Weapon, logic.Owner)) {
-                        RuleCalculateWeaponStats ruleCalculateWeaponStats = Rulebook.Trigger<RuleCalculateWeaponStats>(new RuleCalculateWeaponStats(Game.Instance.DefaultUnit, evt.DamageBundle.Weapon, null));
-                        DiceFormula dice = new DiceFormula(Math.Max(ruleCalculateWeaponStats.CriticalMultiplier - 1, 1), __instance.Dice);
-                        evt.DamageBundle.Add(new EnergyDamage(dice, __instance.Element));
-                    }
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-        }
-
         [HarmonyLib.HarmonyPatch(typeof(WeaponExtraAttack), "OnEventAboutToTrigger")]
         // ReSharper disable once UnusedMember.Local
         private static class WeaponExtraAttackOnEventAboutToTriggerPatch {
