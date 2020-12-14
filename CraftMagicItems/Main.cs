b60c45716ea53b391637ba69446ef9f2db11d62c
Erik Frantz <bardez@bardez.net>
2020-12-14 12:30:05 -0600

    Extracting the Harmony patches from Main into their own files: BlueprintItemEquipmentUsableCostPatch

--- CraftMagicItems/Main.cs
+++ CraftMagicItems/Main.cs
@@ -2889,32 +2889,6 @@ namespace CraftMagicItems {
             return true;
         }
 
-        [HarmonyLib.HarmonyPatch(typeof(BlueprintItemEquipmentUsable), "Cost", HarmonyLib.MethodType.Getter)]
-        // ReSharper disable once UnusedMember.Local
-        private static class BlueprintItemEquipmentUsableCostPatch {
-            // ReSharper disable once UnusedMember.Local
-            private static void Postfix(BlueprintItemEquipmentUsable __instance, ref int __result) {
-                if (__result == 0 && __instance.SpellLevel == 0) {
-                    // Owlcat's cost calculation doesn't handle level 0 spells properly.
-                    int chargeCost;
-                    switch (__instance.Type) {
-                        case UsableItemType.Wand:
-                            chargeCost = 15;
-                            break;
-                        case UsableItemType.Scroll:
-                            chargeCost = 25;
-                            break;
-                        case UsableItemType.Potion:
-                            chargeCost = 50;
-                            break;
-                        default:
-                            return;
-                    }
-                    __result = __instance.CasterLevel * chargeCost * __instance.Charges / 2;
-                }
-            }
-        }
-
         // Load Variant spells into m_KnownSpellLevels
         [HarmonyLib.HarmonyPatch(typeof(Spellbook), "PostLoad")]
         // ReSharper disable once UnusedMember.Local
---
+++ CraftMagicItems/Patches/Harmony/BlueprintItemEquipmentUsableCostPatch.cs
@@ -0,0 +1,35 @@
+﻿using Kingmaker.Blueprints.Items.Equipment;
+
+namespace CraftMagicItems.Patches.Harmony
+{
+    [HarmonyLib.HarmonyPatch(typeof(BlueprintItemEquipmentUsable), "Cost", HarmonyLib.MethodType.Getter)]
+    // ReSharper disable once UnusedMember.Local
+    public static class BlueprintItemEquipmentUsableCostPatch
+    {
+        // ReSharper disable once UnusedMember.Local
+        private static void Postfix(BlueprintItemEquipmentUsable __instance, ref int __result)
+        {
+            if (__result == 0 && __instance.SpellLevel == 0)
+            {
+                // Owlcat's cost calculation doesn't handle level 0 spells properly.
+                int chargeCost;
+                switch (__instance.Type)
+                {
+                    case UsableItemType.Wand:
+                        chargeCost = 15;
+                        break;
+                    case UsableItemType.Scroll:
+                        chargeCost = 25;
+                        break;
+                    case UsableItemType.Potion:
+                        chargeCost = 50;
+                        break;
+                    default:
+                        return;
+                }
+
+                __result = __instance.CasterLevel * chargeCost * __instance.Charges / 2;
+            }
+        }
+    }
+}
