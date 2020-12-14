64fc8afebc3ba1f308dd10422573e55345679ea8
Erik Frantz <bardez@bardez.net>
2020-12-14 13:07:24 -0600

    Extracting the Harmony patches from Main into their own files: UIUtilityItemFillArmorEnchantmentsPatch

--- CraftMagicItems/Main.cs
+++ CraftMagicItems/Main.cs
@@ -3333,32 +3333,6 @@ namespace CraftMagicItems {
             }
         }
 
-        [HarmonyLib.HarmonyPatch(typeof(UIUtilityItem), "FillArmorEnchantments")]
-        // ReSharper disable once UnusedMember.Local
-        private static class UIUtilityItemFillArmorEnchantmentsPatch {
-            // ReSharper disable once UnusedMember.Local
-            private static void Postfix(TooltipData data, ItemEntityShield armor) {
-                if (armor.IsIdentified) {
-                    foreach (var itemEnchantment in armor.Enchantments) {
-                        itemEnchantment.Blueprint.CallComponents<AddStatBonusEquipment>(c => {
-                            if (c.Descriptor != ModifierDescriptor.ArmorEnhancement && c.Descriptor != ModifierDescriptor.ShieldEnhancement && !data.StatBonus.ContainsKey(c.Stat)) {
-                                data.StatBonus.Add(c.Stat, UIUtility.AddSign(c.Value));
-                            }
-                        });
-                        var component = itemEnchantment.Blueprint.GetComponent<AllSavesBonusEquipment>();
-                        if (component != null) {
-                            StatType[] saves = { StatType.SaveReflex, StatType.SaveWill, StatType.SaveFortitude };
-                            foreach (var save in saves) {
-                                if (!data.StatBonus.ContainsKey(save)) {
-                                    data.StatBonus.Add(save, UIUtility.AddSign(component.Value));
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
         [HarmonyLib.HarmonyPatch(typeof(UIUtilityItem), "FillEnchantmentDescription")]
         // ReSharper disable once UnusedMember.Local
         private static class UIUtilityItemFillEnchantmentDescriptionPatch {
---
+++ CraftMagicItems/Patches/Harmony/UIUtilityItemFillArmorEnchantmentsPatch.cs
@@ -0,0 +1,46 @@
+﻿using Kingmaker.Blueprints;
+using Kingmaker.Designers.Mechanics.EquipmentEnchants;
+using Kingmaker.EntitySystem.Stats;
+using Kingmaker.Enums;
+using Kingmaker.Items;
+using Kingmaker.UI.Common;
+using Kingmaker.UI.Tooltip;
+
+namespace CraftMagicItems.Patches.Harmony
+{
+    [HarmonyLib.HarmonyPatch(typeof(UIUtilityItem), "FillArmorEnchantments")]
+    // ReSharper disable once UnusedMember.Local
+    public static class UIUtilityItemFillArmorEnchantmentsPatch
+    {
+        // ReSharper disable once UnusedMember.Local
+        private static void Postfix(TooltipData data, ItemEntityShield armor)
+        {
+            if (armor.IsIdentified)
+            {
+                foreach (var itemEnchantment in armor.Enchantments)
+                {
+                    itemEnchantment.Blueprint.CallComponents<AddStatBonusEquipment>(c =>
+                    {
+                        if (c.Descriptor != ModifierDescriptor.ArmorEnhancement && c.Descriptor != ModifierDescriptor.ShieldEnhancement && !data.StatBonus.ContainsKey(c.Stat))
+                        {
+                            data.StatBonus.Add(c.Stat, UIUtility.AddSign(c.Value));
+                        }
+                    });
+
+                    var component = itemEnchantment.Blueprint.GetComponent<AllSavesBonusEquipment>();
+                    if (component != null)
+                    {
+                        StatType[] saves = { StatType.SaveReflex, StatType.SaveWill, StatType.SaveFortitude };
+                        foreach (var save in saves)
+                        {
+                            if (!data.StatBonus.ContainsKey(save))
+                            {
+                                data.StatBonus.Add(save, UIUtility.AddSign(component.Value));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
